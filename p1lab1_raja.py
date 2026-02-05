b93099b6b7169bf9c2d8ff3118aa73a42a3bb64e
HipHopNoah <noahrajaschool@gmail.com>
2026-02-05 15:37:55 +0000

    Worked on the Lab

---
+++ p1lab1_raja.py
@@ -0,0 +1,11 @@
+# CTI 110 
+# P1LAB1 
+# Noah Raja 
+# 2/5/2026
+
+# Enter Name
+item_first = input("Enter your first name:")
+item_last = input("Enter your last name:")
+
+# Print output 😜
+print(f"Hello, {item_first} {item_last}! Welcome to CT1-110")
