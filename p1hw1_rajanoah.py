c00efd28de89d136f57d5625fabd54b5f8773d7d
HipHopNoah <noahrajaschool@gmail.com>
2026-02-10 11:12:37 -0500

    Update p1hw1_rajanoah.py

--- p1hw1_rajanoah.py
+++ p1hw1_rajanoah.py
@@ -3,9 +3,21 @@
 # P1HW1
 # Creating math equations with python
 
+print("-----Calculating Exponents-----")
 base = int(input("Enter an integer as the base value: "))
 exponent = int(input("Enter an integer as the exponent: "))
 
 potato = base ** exponent 
 
 print(f"{base} raised to the power of {exponent} is {potato} !! Bam What")
+
+print()
+print("-----Addition and Subtraction-----")
+
+int1 = int(input("Enter a starting integer: "))
+int2 = int(input("Enter an integer to add: "))
+int3 = int(input("Enter an integer to subtract: "))
+
+tomato = int1 + int2 - int3
+
+print(f"{int1} + {int2} - {int3} is equal to {tomato}")
