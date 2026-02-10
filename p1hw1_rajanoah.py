0a74f601e28db739f7bbd6439029f48c3302883f
HipHopNoah <noahrajaschool@gmail.com>
2026-02-10 11:05:24 -0500

    Create p1hw1_rajanoah.py

---
+++ p1hw1_rajanoah.py
@@ -0,0 +1,11 @@
+# Noah Raja
+# 2/10/2026
+# P1HW1
+# Creating math equations with python
+
+base = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+
+potato = base ** exponent 
+
+print(f"{base} raised to the power of {exponent} is {potato} !! Bam What")
