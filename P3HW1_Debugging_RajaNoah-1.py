7498ce2832b39f51b61a082901d74d3a59ad3faa
HipHopNoah <noahrajaschool@gmail.com>
2026-03-05 10:36:28 -0500

    Add files via upload

---
+++ P3HW1_Debugging_RajaNoah-1.py
@@ -0,0 +1,56 @@
+# Noah Raja
+# 3/5/2026
+# P3HW1
+# Calculating a Gradebook
+
+
+# This program takes a number grade , determines average and displays letter grade for average.
+
+# Enter grades for six modules
+
+mod_1 = float(input("Enter grade for Module 1: "))
+mod_2 = float(input("Enter grade for Module 2: "))
+mod_3 = float(input("Enter grade for Module 3: "))
+mod_4 = float(input("Enter grade for Module 4: "))
+mod_5 = float(input("Enter grade for Module 5: "))
+mod_6 = float(input("Enter grade for Module 6: "))
+
+# add grades entered to a list
+
+grades = [mod_1, mod_2, mod_3, mod_4, mod_5, mod_6]
+# TO DO: determine lowest, highest , sum and average for grades
+
+low = min(grades)
+high = max(grades)
+sum = sum(grades)
+length = len(grades)
+avg = sum/length
+
+print("------------Results------------")
+
+print(f"{'Lowest Grade:':<21} {low:.1f}")
+print(f"{'Highest Grade:':<21} {high:.1f}")
+print(f"{'Sum of Grades:':<21} {sum:.1f}")
+print(f"{'Average:':<21} {avg:.2f}")
+
+print("-------------------------------")
+
+# determine letter grade for average
+
+
+if avg >= 90:
+    print('Your grade is: A')
+elif avg < 70:
+    print('Your grade is: F')
+elif avg >= 70:
+    print('Your grade is: C')
+elif avg >= 80:
+    print('Your grade is: B')
+
+
+
+
+
+
+
+
